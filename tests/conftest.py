1ed581dd0058af159315c9ce56c824be3b5047da
Oliver Josem <omjosem@gmail.com>
2021-07-04 16:24:56 +1000

    Refactor pytest fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+from tinydb import TinyDB
+
+
+
+@pytest.fixture(scope="module")
+def db_dir(tmp_path_factory):
+    """A temp dir for the db"""
+    return tmp_path_factory.mktemp("tmp_db_dir")
+
+
+@pytest.fixture(scope="module")
+def db_path(db_dir):
+    """A db that can be used for all tests"""
+    return db_dir / "db.json"
+
+
+@pytest.fixture(scope="module")
+def db_module(db_path):
+    """A db that can be used for all tests"""
+    return TinyDB(db_path)
+
+
+@pytest.fixture()
+def robot_db(db_module):
+    db_module.truncate()
+    return db_module
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,36 +1,8 @@
-from tinydb.database import TinyDB
 from robot import __version__, Robot
-from tinydb import TinyDB
-
-import pytest
 
 
 def test_version():
     assert __version__ == "0.1.0"
-
-
-@pytest.fixture(scope="module")
-def db_dir(tmp_path_factory):
-    """A temp dir for the db"""
-    return tmp_path_factory.mktemp("tmp_db_dir")
-
-
-@pytest.fixture(scope="module")
-def db_path(db_dir):
-    """A db that can be used for all tests"""
-    return db_dir / "db.json"
-
-
-@pytest.fixture(scope="module")
-def db_module(db_path):
-    """A db that can be used for all tests"""
-    return TinyDB(db_path)
-
-
-@pytest.fixture()
-def robot_db(db_module):
-    db_module.truncate()
-    return db_module
 
 
 def test_place(robot_db, db_path):
